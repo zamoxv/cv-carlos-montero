14ec567d8c62e03e3d88b28721e8cc9dbd152d1f
Carlos Montero <zamoxv@gmail.com>
2025-12-03 12:41:18 -0300

    Se agregan enlaces a proyectos

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 let currentLang = "es";
 
 function applyLanguage() {
-  // Rellena todos los textos según el idioma actual
-  document.querySelectorAll("[data-es]").forEach(el => {
+  document.querySelectorAll("[data-es]", "[data-en]").forEach(el => {
     const text = el.getAttribute(`data-${currentLang}`);
     if (text) {
-      el.innerText = text;
+      el.innerHTML = text;
     }
   });
 
-  // Actualiza el texto del botón
   const btn = document.getElementById("lang-btn");
   if (btn) {
     btn.innerText = currentLang === "es" ? "🌐 Español" : "🌐 English";
@@ -17,17 +15,13 @@ function applyLanguage() {
 }
 
 function toggleLanguage() {
-  // Alternar idioma
   currentLang = currentLang === "es" ? "en" : "es";
   applyLanguage();
 }
 
-// Cuando cargue la página:
 document.addEventListener("DOMContentLoaded", () => {
-  // 1) Aplicamos el idioma inicial (español)
   applyLanguage();
 
-  // 2) Conectamos el botón
   const btn = document.getElementById("lang-btn");
   if (btn) {
     btn.addEventListener("click", toggleLanguage);
