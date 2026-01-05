ff9e1b14928311b57dcd19172f1b06b73139491b
zamox <zamoxv@gmail.com>
2026-01-04 23:20:20 -0300

    New bilingual PDF, new preject's description and info

--- script.js
+++ script.js
@@ -17,13 +17,32 @@ function applyLanguage() {
 function toggleLanguage() {
   currentLang = currentLang === "es" ? "en" : "es";
   applyLanguage();
+  updateCVLink(currentLang);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   applyLanguage();
+  updateCVLink(currentLang);
 
   const btn = document.getElementById("lang-btn");
   if (btn) {
     btn.addEventListener("click", toggleLanguage);
   }
 });
+
+const cvLink = document.getElementById("cv-download");
+
+function updateCVLink(currentLang) {
+  const link = document.getElementById("cv-download");
+  const tooltip = link.querySelector(".tooltip");
+
+  if (currentLang === "es") {
+    link.href = "cv/Carlos_Montero_CV_ES.pdf";
+    link.firstChild.textContent = "📄 Descargar CV (PDF)";
+    tooltip.textContent = "Se descargará el CV en español";
+  } else {
+    link.href = "cv/Carlos_Montero_CV_EN.pdf";
+    link.firstChild.textContent = "📄 Download CV (PDF)";
+    tooltip.textContent = "The CV will be downloaded in English";
+  }
+}
